be43a43b868ccfc5adf5787161e5a2bde479fbc8
Kurtina <kurtdekker@gmail.com>
2018-05-24 07:45:56 -0700

    DSAudioVolume and DSAudioPlay: demo these items in the main scene

--- datasacks/Assets/Datasack/DSMCodegen.cs
+++ datasacks/Assets/Datasack/DSMCodegen.cs
@@ -11,6 +11,7 @@
 //
 public partial class DSM
 {
+	public static Datasack AudioSourceHello { get { return DSM.I.Get( "AudioSourceHello"); } }
 	public static Datasack GameRunning { get { return DSM.I.Get( "GameRunning"); } }
 	public static Datasack GUID { get { return DSM.I.Get( "GUID"); } }
 	public static Datasack HardMode { get { return DSM.I.Get( "HardMode"); } }
