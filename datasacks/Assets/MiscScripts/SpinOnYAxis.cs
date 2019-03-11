5f955ddb7c7e3fb90f6549822cbbf14232374ef4
Kurt Dekker <kdekker@farcaster>
2019-03-11 15:47:07 -0700

    SpinOnYAxis script

---
+++ datasacks/Assets/MiscScripts/SpinOnYAxis.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinOnYAxis : MonoBehaviour
+{
+	public float RateOfSpin;
+
+	float age;
+
+	void Reset()
+	{
+		RateOfSpin = 100.0f;
+	}
+
+	void Update ()
+	{
+		age += Time.deltaTime;
+		transform.localRotation = Quaternion.Euler( 0, age * RateOfSpin, 0);
+	}
+}
