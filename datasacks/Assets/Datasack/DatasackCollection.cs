af5350676587eb6001346124208a3287b9c9777c
Kurt Dekker <kdekker@endymion>
2019-05-19 15:14:56 -0700

    DatasackCollection: supply ordering for our headers

--- datasacks/Assets/Datasack/DatasackCollection.cs
+++ datasacks/Assets/Datasack/DatasackCollection.cs
@@ -11,12 +11,12 @@ public class DatasackCollection : ScriptableObject
 		public Datasack Datasack;
 	}
 
-	[Header("This holds ALL datasacks in your project.")]
+	[Header("This holds ALL datasacks in your project.", order = 100)]
 
-	[Header("Do not modify it yourself!")]
+	[Header("Do not modify it yourself!", order = 200)]
 
-	[Header("Instead use CODEGEN from any")]
-	[Header("Datasack object to regenerate.")]
+	[Header("Instead use CODEGEN from any", order = 300)]
+	[Header("Datasack object to regenerate.", order = 310)]
 
 	public	DatasackMapping[] Mappings;
 }
