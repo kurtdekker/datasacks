9d2e6a7f028f7bbe1bf44215472d10097b5ffd84
Kurt Dekker <kdekker@endymion>
2019-08-22 11:46:41 -0700

    DSCameraControl created.

---
+++ datasacks/Assets/Datasack/Control/DSCameraControl.cs
@@ -0,0 +1,85 @@
+﻿/*
+	The following license supersedes all notices in the source code.
+
+	Copyright (c) 2019 Kurt Dekker/PLBM Games All rights reserved.
+
+	http://www.twitter.com/kurtdekker
+
+	Redistribution and use in source and binary forms, with or without
+	modification, are permitted provided that the following conditions are
+	met:
+
+	Redistributions of source code must retain the above copyright notice,
+	this list of conditions and the following disclaimer.
+
+	Redistributions in binary form must reproduce the above copyright
+	notice, this list of conditions and the following disclaimer in the
+	documentation and/or other materials provided with the distribution.
+
+	Neither the name of the Kurt Dekker/PLBM Games nor the names of its
+	contributors may be used to endorse or promote products derived from
+	this software without specific prior written permission.
+
+	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+	IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+	TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+	PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+	HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+	SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+	TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+	PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+	LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+	NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+	SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DSCameraControl : MonoBehaviour
+{
+	[Header( "Only supply the Camera properties you want to control:")]
+	public	Datasack	DataSackFOV;
+	public	Datasack	DataSackOrthoSize;
+
+	Camera cam;
+
+	void	OnChangedFOV( Datasack ds)
+	{
+		cam.fieldOfView = ds.fValue;
+	}
+
+	void	OnChangedOrthoSize( Datasack ds)
+	{
+		cam.orthographicSize = ds.fValue;
+	}
+
+	void	OnEnable()
+	{
+		cam = GetComponent<Camera>();
+
+		if (DataSackFOV)
+		{
+			DataSackFOV.OnChanged += OnChangedFOV;	
+			OnChangedFOV(DataSackFOV);
+		}
+		if (DataSackOrthoSize)
+		{
+			DataSackOrthoSize.OnChanged += OnChangedOrthoSize;	
+			OnChangedOrthoSize(DataSackOrthoSize);
+		}
+	}
+	void	OnDisable()
+	{
+		if (DataSackFOV)
+		{
+			DataSackFOV.OnChanged -= OnChangedFOV;
+		}
+		if (DataSackOrthoSize)
+		{
+			DataSackOrthoSize.OnChanged -= OnChangedOrthoSize;
+		}
+	}
+}
