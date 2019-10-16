1a8f96e6696d230a878ba2e34786de3017b18f94
Kurtina <kurtdekker@gmail.com>
2019-10-16 06:50:00 -0700

    DSTextSelectInt: ability to select string output based on an integer index

---
+++ datasacks/Assets/Datasack/Control/DSTextSelectInt.cs
@@ -0,0 +1,92 @@
+﻿/*
+	The following license supersedes all notices in the source code.
+
+	Copyright (c) 2019 Kurt Dekker/PLBM Games All rights reserved.
+
+	http://www.twitter.com/kurtdekker
+
+	Redistribution and use in source and binary forms, with or without
+	modification, are permitted provided that the following conditions are
+	met:
+
+	Redistributions of source code must retain the above copyright notice,
+	this list of conditions and the following disclaimer.
+
+	Redistributions in binary form must reproduce the above copyright
+	notice, this list of conditions and the following disclaimer in the
+	documentation and/or other materials provided with the distribution.
+
+	Neither the name of the Kurt Dekker/PLBM Games nor the names of its
+	contributors may be used to endorse or promote products derived from
+	this software without specific prior written permission.
+
+	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+	IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+	TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+	PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+	HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+	SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+	TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+	PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+	LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+	NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+	SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DSTextSelectInt : MonoBehaviour
+{
+	public	Datasack	dataSack;
+
+	public	string[]	TextTable;
+
+	[Multiline]
+	public	string		FormatString;
+
+	private DSTextAbstraction _textAbstraction;
+	private DSTextAbstraction textAbstraction
+	{
+		get
+		{
+			if (!_textAbstraction) _textAbstraction = DSTextAbstraction.Attach(this);
+			return _textAbstraction;
+		}
+	}
+
+	void	Reset()
+	{
+		TextTable = new string[] { "Option 0", "Option 1" };
+	}
+
+	void	OnChanged( Datasack ds)
+	{
+		int n = ds.iValue;
+
+		string text = "";
+		if (n >= 0 && n < TextTable.Length)
+		{
+			text = TextTable[n];
+		}
+
+		if (!System.String.IsNullOrEmpty(FormatString))
+		{
+			textAbstraction.SetText( System.String.Format (FormatString, text));
+			return;
+		}
+		textAbstraction.SetText( text);
+	}
+
+	void	OnEnable()
+	{
+		dataSack.OnChanged += OnChanged;
+		OnChanged( dataSack);
+	}
+	void	OnDisable()
+	{
+		dataSack.OnChanged -= OnChanged;	
+	}
+}
