0aa5fad490df9da034c6d5672701ccb533e9d5f0
Kurt Dekker <kdekker@endymion>
2020-01-28 17:44:16 -0800

    DSStateMachineBehaviour: a simple generic way to respond to Animator State Machine change events

---
+++ datasacks/Assets/Datasack/Miscellaneous/DSStateMachineBehaviour.cs
@@ -0,0 +1,81 @@
+﻿/*
+	The following license supersedes all notices in the source code.
+
+	Copyright (c) 2020 Kurt Dekker/PLBM Games All rights reserved.
+
+	http://www.twitter.com/kurtdekker
+
+	Redistribution and use in source and binary forms, with or without
+	modification, are permitted provided that the following conditions are
+	met:
+
+	Redistributions of source code must retain the above copyright notice,
+	this list of conditions and the following disclaimer.
+
+	Redistributions in binary form must reproduce the above copyright
+	notice, this list of conditions and the following disclaimer in the
+	documentation and/or other materials provided with the distribution.
+
+	Neither the name of the Kurt Dekker/PLBM Games nor the names of its
+	contributors may be used to endorse or promote products derived from
+	this software without specific prior written permission.
+
+	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+	IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+	TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+	PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+	HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+	SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+	TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+	PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+	LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+	NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+	SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DSStateMachineBehaviour : StateMachineBehaviour
+{
+	[Tooltip("Defaults to UserIntent datasack if none supplied.")]
+	public Datasack dsOnStateEnter;
+	[Tooltip("Leave blank to ignore OnStateEnter signal.")]
+	public string valueOnStateEnter;
+
+	[Tooltip("Defaults to UserIntent datasack if none supplied.")]
+	public Datasack dsOnStateExit;
+	[Tooltip("Leave blank to ignore OnStateExit signal.")]
+	public string valueOnStateExit;
+
+	void Reset()
+	{
+		valueOnStateEnter = "";
+		valueOnStateExit = name + "OnStateExit";
+	}
+
+	private void SignalEvent(Datasack ds, string signallingValue)
+	{
+		if (!string.IsNullOrEmpty( signallingValue))
+		{
+			if (!ds) ds = DSM.UserIntent;
+
+			ds.Value = signallingValue;
+		}
+	}
+
+	public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+	{
+		base.OnStateEnter(animator, stateInfo, layerIndex);
+
+		SignalEvent(dsOnStateEnter, valueOnStateEnter);
+	}
+
+	public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+	{
+		base.OnStateExit(animator, stateInfo, layerIndex);
+
+		SignalEvent(dsOnStateExit, valueOnStateExit);
+	}
+}
