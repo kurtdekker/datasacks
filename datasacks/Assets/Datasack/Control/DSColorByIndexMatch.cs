c6d4385556e092395944f30927de616c0198d3bd
Kurtina <kurtdekker@gmail.com>
2024-10-14 16:22:59 -0700

    DSColorByIndexMatch: handy stuff!

---
+++ datasacks/Assets/Datasack/Control/DSColorByIndexMatch.cs
@@ -0,0 +1,108 @@
+﻿/*
+	The following license supersedes all notices in the source code.
+
+	Copyright (c) 2024 Kurt Dekker/PLBM Games All rights reserved.
+
+	http://www.twitter.com/kurtdekker
+
+	Redistribution and use in source and binary forms, with or without
+	modification, are permitted provided that the following conditions are
+	met:
+
+	Redistributions of source code must retain the above copyright notice,
+	this list of conditions and the following disclaimer.
+
+	Redistributions in binary form must reproduce the above copyright
+	notice, this list of conditions and the following disclaimer in the
+	documentation and/or other materials provided with the distribution.
+
+	Neither the name of the Kurt Dekker/PLBM Games nor the names of its
+	contributors may be used to endorse or promote products derived from
+	this software without specific prior written permission.
+
+	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+	IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+	TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+	PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+	HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+	SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+	TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+	PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+	LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+	NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+	SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+// Purpose: injects the .colorValue from a Datasack into a colorable.
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+// Based on the datasack (integer .iValue) matching
+// the sibling index (or a constant), tells the
+// colorable abstraction to be one of two colors.
+//
+// The intention is to show you are selected.
+
+public class DSColorByIndexMatch : MonoBehaviour
+{
+	public	Datasack	dataSack;
+
+	[Header("What do we have to match?")]
+	public	int			IndexToMatch;
+
+	[Header("Overrides the above index.")]
+	public	bool		UseSiblingIndex;
+
+	[Header( "Colors for non-match and match:")]
+	public	Color		NonMatchingColor;
+	public	Color		MatchingColor;
+
+	private	DSColorableAbstraction _colorable;
+	private	DSColorableAbstraction colorable
+	{
+		get
+		{
+			if (!_colorable) _colorable = DSColorableAbstraction.Attach( this);
+			return _colorable;
+		}
+	}
+
+	void Start ()
+	{
+		OnChanged (dataSack);
+	}
+
+	void	OnChanged( Datasack ds)
+	{
+		int dsIndex = ds.iValue;
+
+		int ourIndex = IndexToMatch;
+
+		if (UseSiblingIndex)
+		{
+			ourIndex = transform.GetSiblingIndex();
+		}
+
+		Color c = NonMatchingColor;
+
+		if (ourIndex == dsIndex)
+		{
+			c = MatchingColor;
+		}
+
+		colorable.SetColor( c);
+	}
+
+	void	OnEnable()
+	{
+		dataSack.OnChanged += OnChanged;	
+		OnChanged(dataSack);
+	}
+	void	OnDisable()
+	{
+		dataSack.OnChanged -= OnChanged;	
+	}
+}
