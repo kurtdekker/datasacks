facba05417383365de6f2cfbd9120651a67e2317
Kurtimon <kdekker@kurtimon>
2025-01-18 09:08:58 -0800

    StringCollection.cs: some handy methods from my MECHA game

--- datasacks/Assets/Datasack/Core/StringCollection.cs
+++ datasacks/Assets/Datasack/Core/StringCollection.cs
@@ -5,5 +5,29 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Collections/StringCollection")]
 public class StringCollection : ScriptableObject
 {
+	[Header( "This is just for my edification.")]
+	[Multiline]
+	public string Description;
+
+	[Space]
+
 	public string[] Strings;
+
+	public int Length
+	{
+		get
+		{
+			if (Strings != null)
+			{
+				return Strings.Length;
+			}
+			return 0;
+		}
+	}
+
+	public string GetString( int i)
+	{
+		// no bounding! That's on you...
+		return Strings[i];
+	}
 }
