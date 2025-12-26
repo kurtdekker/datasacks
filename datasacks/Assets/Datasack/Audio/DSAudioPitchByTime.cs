01e8799354f5b5356356f698ccf1b7b06287fc72
Kurtimon <kdekker@kurtimon>
2025-12-26 07:18:04 -0800

    DSAudioPitchByTime: see comments for difference from base DSAudioPitch class

---
+++ datasacks/Assets/Datasack/Audio/DSAudioPitchByTime.cs
@@ -0,0 +1,54 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+// 5:11 PM 12/24/2025 - Merry Christmas!
+//
+// Differences:
+//
+// DSAudioPitch sets the pitch
+//
+// DSAudioPitchByTime sets the pitch to play each
+//	AudioClip out in the float-specified duration.
+//
+public class DSAudioPitchByTime : MonoBehaviour
+{
+	[Header( "Signal time in seconds into here:")]
+	public	Datasack	dataSack;
+
+	private AudioSource[] azzs;
+
+	void Start ()
+	{
+		OnChanged (dataSack);
+	}
+
+	void	OnChanged( Datasack ds)
+	{
+		float playbackDuration = ds.fValue;
+
+		if (playbackDuration >= 0.1f)
+		{
+			foreach (var az in azzs)
+			{
+				float sampleDuration = az.clip.length;
+
+				float pitch = sampleDuration / playbackDuration;
+
+				az.pitch = pitch;
+			}
+		}
+	}
+
+	void	OnEnable()
+	{
+		azzs = GetComponentsInChildren<AudioSource>();
+		dataSack.OnChanged += OnChanged;	
+	}
+	void	OnDisable()
+	{
+		dataSack.OnChanged -= OnChanged;	
+	}
+}
